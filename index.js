3b07268b89743e71d9ac9c844d0ce5fb1e240bd2
Your Name <abdullahi.abass@student.moringschool.com>
2023-06-30 20:20:06 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const main = document.querySelector('main#main');
-main.remove();
+// Remove the <main> element with id "main"
+const mainElement = document.getElementById("main");
+mainElement.remove();
+
+// Create a variable named newHeader and assign it the <h1> element with id "victory"
 const newHeader = document.createElement("h1");
 newHeader.id = "victory";
-newHeader.textContent = "YOUR-NAME is the champion";c
+
+// Set the text content of the newHeader element
+newHeader.textContent = "YOUR-NAME is the champion";
+
+// Append the newHeader element to the document body
 document.body.appendChild(newHeader);
