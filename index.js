fcb1e12c783bc947d8172c70acc3395805804870
Your Name <abdullahi.abass@student.moringschool.com>
2023-06-30 20:15:48 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ const main = document.querySelector('main#main');
 main.remove();
 const newHeader = document.createElement("h1");
 newHeader.id = "victory";
+newHeader.textContent = "YOUR-NAME is the champion";
